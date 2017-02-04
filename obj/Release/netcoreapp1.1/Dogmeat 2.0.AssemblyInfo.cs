558d576c04154a22726c3bcc60a2d481a77164a6
Kr4ken-9 <Kr4kenKT@gmail.com>
2017-02-03 18:25:53 -0600

    Build for AppVeyor

--- obj/Release/netcoreapp1.1/Dogmeat 2.0.AssemblyInfo.cs
+++ obj/Release/netcoreapp1.1/Dogmeat 2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dogmeat 2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.2")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dogmeat 2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dogmeat 2.0")]
