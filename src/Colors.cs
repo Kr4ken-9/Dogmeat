8ce0c44abfb37027c0319a939f38a07b77db8332
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2018-01-22 21:08:22 -0800

    make colors readonly

--- src/Colors.cs
+++ src/Colors.cs
@@ -4,12 +4,12 @@ namespace Dogmeat
 {
     public class Colors
     {
-        public static Color SexyPurple = new Color(153, 0, 255);
+        public static readonly Color SexyPurple = new Color(153, 0, 255);
         
-        public static Color SexyBlue = new Color(0, 112, 255);
+        public static readonly Color SexyBlue = new Color(0, 112, 255);
         
-        public static Color SexyLime = new Color(10, 255, 0);
+        public static readonly Color SexyLime = new Color(10, 255, 0);
         
-        public static Color SexyOrange = new Color(255, 123, 2);
+        public static readonly Color SexyOrange = new Color(255, 123, 2);
     }
 }
