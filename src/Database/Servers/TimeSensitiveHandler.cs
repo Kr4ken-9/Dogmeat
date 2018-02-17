3b945a6d799a1b54ff082f5c611c17eddbfbf115
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2018-02-16 18:19:48 -0800

    fix ban checker not working

--- src/Database/Servers/TimeSensitiveHandler.cs
+++ src/Database/Servers/TimeSensitiveHandler.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Discord.WebSocket;
 using Dogmeat.Utilities;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Dogmeat.Database.Servers
 {
@@ -17,22 +18,26 @@ namespace Dogmeat.Database.Servers
                 using (DatabaseHandler Context = new DatabaseHandler())
                 {
                     await Context.Database.EnsureCreatedAsync();
-                    
+
                     var TempBans = Context.TempBans.Where(t => t.UnbanTime <= Vars.Now());
                     if (await TempBans.AnyAsync())
                     {
                         foreach (TempBan Ban in TempBans)
                         {
                             SocketGuild Guild = Vars.Client.GetGuild(Ban.ServerID);
-                            await Guild.RemoveBanAsync(Ban.ID);
-
                             SocketUser User = Vars.Client.GetUser(Ban.ID);
+
+                            if (User == null || Guild == null)
+                                continue;
+
+                            try { await Guild.RemoveBanAsync(Ban.ID); }
+                            catch (Discord.Net.HttpException e) { }
+
                             (await User.GetOrCreateDMChannelAsync()).SendMessageAsync(
                                 $"You have been unbanned from {Guild.Name}");
 
                             Context.TempBans.Remove(Ban); //TODO: Why does this hang thread?!?!
-                            await Context.SaveChangesAsync();
-                            
+
                             Logger.Log($"Unbanned {User.Username}");
                         }
                     }
@@ -52,8 +57,9 @@ namespace Dogmeat.Database.Servers
                             Context.Reminders.Remove(Reminder);
                         }
                     }
-                    
+
                     await Context.SaveChangesAsync();
+
                 }
                 Logger.Log("Time Sensitives Checked");
                 Thread.Sleep(60000);
